95b8430a936004591fb05a9907d48fe5019bd080
Georgios <georgios@ip-192-168-40-26.lon.mindcandy.com>
2016-11-01 14:37:49 +0000

    Add Resource AWS - VPC - Internet Gateway

---
+++ aws-vpc-internet_gateways.tf
@@ -0,0 +1,13 @@
+resource "aws_internet_gateway" "gw" {
+  count   = "${var.aws_vpc_app_enabled}"
+  vpc_id  = "${aws_vpc.app.id}"
+
+  tags {
+    Name          = "VPC-iGateway-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    Resource      = "InternetGateway"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
+  }
+}
