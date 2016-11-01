f2ba9fd1b0d9b25200d0eca9e0b6b0c3217c9fa3
gfisaris <gfisaris@gmail.com>
2016-11-01 15:09:34 +0000

    Seperate Network ACL Rules for Public and Private Subnets

--- aws-vpc-network_acl.tf
+++ aws-vpc-network_acl.tf
@@ -1,4 +1,11 @@
 resource "aws_network_acl" "public" {
-  count   = "${var.aws_vpc_app_enabled}"
-  vpc_id  = "${aws_vpc.app.id}"
+  count       = "${var.aws_vpc_app_enabled}"
+  vpc_id      = "${aws_vpc.app.id}"
+  subnet_ids  = ["${aws_subnet.public.*.id}"]
+}
+
+resource "aws_network_acl" "private" {
+  count       = "${var.aws_vpc_app_enabled}"
+  vpc_id      = "${aws_vpc.app.id}"
+  subnet_ids  = ["${aws_subnet.private.*.id}"]
 }
--- aws-vpc-network_acl_rules.tf
+++ aws-vpc-network_acl_rules.tf
@@ -1,15 +1,4 @@
-/*
-resource "aws_network_acl_rule" "ingress_deny_all" {
-  count           = "${var.aws_vpc_app_enabled}"
-  network_acl_id  = "${aws_network_acl.public.id}"
-  rule_number     = "*"
-  rule_action     = "deny"
-  egress          = false
-  protocol        = "all"    
-  cidr_block      = "0.0.0.0/0"
-}
-*/
-resource "aws_network_acl_rule" "ingress_allow_all" {
+resource "aws_network_acl_rule" "public-ingress_allow_all" {
   count           = "${var.aws_vpc_app_enabled}"
   network_acl_id  = "${aws_network_acl.public.id}"
   rule_number     = "100"
@@ -18,23 +7,34 @@ resource "aws_network_acl_rule" "ingress_allow_all" {
   protocol        = "all"    
   cidr_block      = "0.0.0.0/0"
 }
-/*
-resource "aws_network_acl_rule" "egress_deny_all" {
-  count           = "${var.aws_vpc_app_enabled}"
-  network_acl_id  = "${aws_network_acl.public.id}"
-  rule_number     = "*"
-  rule_action     = "deny"
-  egress          = true
-  protocol        = "all"    
-  cidr_block      = "0.0.0.0/0"
-}
-*/
-resource "aws_network_acl_rule" "egress_allow_all" {
+
+resource "aws_network_acl_rule" "public-egress_allow_all" {
   count           = "${var.aws_vpc_app_enabled}"
   network_acl_id  = "${aws_network_acl.public.id}"
   rule_number     = "100"
   rule_action     = "allow"
   egress          = true
   protocol        = "all"    
+  cidr_block      = "0.0.0.0/0"
+}
+
+
+resource "aws_network_acl_rule" "private-ingress_allow_all" {
+  count           = "${var.aws_vpc_app_enabled}"
+  network_acl_id  = "${aws_network_acl.private.id}"
+  rule_number     = "100"
+  rule_action     = "allow"
+  egress          = false
+  protocol        = "all"
+  cidr_block      = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "private-egress_allow_all" {
+  count           = "${var.aws_vpc_app_enabled}"
+  network_acl_id  = "${aws_network_acl.private.id}"
+  rule_number     = "100"
+  rule_action     = "allow"
+  egress          = true
+  protocol        = "all"
   cidr_block      = "0.0.0.0/0"
 }
