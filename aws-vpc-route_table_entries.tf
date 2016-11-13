ac31fba03a23052edc7d35f874eb2b6ce54f02ca
gfisaris <gfisaris@gmail.com>
2016-11-13 09:49:17 +0000

    Update AWS Resources (RouteTable Associations and RouteTables Entries)

--- aws-vpc-route_table_entries.tf
+++ aws-vpc-route_table_entries.tf
@@ -1,13 +1,13 @@
-resource "aws_route" "igateway" {
-  count                   = "${var.aws_vpc_app_enabled}"
-  route_table_id          = "${aws_route_table.public.id}"
-  gateway_id              = "${aws_internet_gateway.master.id}"
+resource "aws_route" "internet_gateway" {
+  count                   = "${var.aws_vpc_ecs_cluster_enabled}"
+  route_table_id          = "${aws_route_table.ecs_cluster_public.id}"
+  gateway_id              = "${aws_internet_gateway.ecs_cluster_vpc.id}"
   destination_cidr_block  = "0.0.0.0/0"
 }
 
 resource "aws_route" "nat_gateway" {
-  count                   = "${var.aws_vpc_app_enabled}"
-  route_table_id          = "${aws_route_table.private.id}"
-  gateway_id              = "${aws_nat_gateway.master.id}"
+  count                   = "${var.aws_vpc_ecs_cluster_enabled}"
+  route_table_id          = "${aws_route_table.ecs_cluster_private.id}"
+  gateway_id              = "${aws_nat_gateway.ecs_cluster_vpc.id}"
   destination_cidr_block  = "0.0.0.0/0"
 }
--- aws-vpc-route_tables.tf
+++ aws-vpc-route_tables.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "ecs_cluster_public" {
 output "ecs_cluster_public_vpc_id" {value = ["${aws_vpc.ecs_cluster.id}"]}
 output "ecs_cluster_public_vpc_route_table_id" {value ="${aws_route_table.ecs_cluster_public.id}"}
 
-resource "aws_route_table" "ecs_cluster_purivate" {
+resource "aws_route_table" "ecs_cluster_private" {
   count   = "${var.aws_vpc_ecs_cluster_enabled}"
   vpc_id  = "${aws_vpc.ecs_cluster.id}"
 
